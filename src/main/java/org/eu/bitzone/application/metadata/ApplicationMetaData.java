23612f51c9b9c9e2b37cb9a7c2e9e3b83b1d3948
wpadula <wojtek.padula@mapflow.local>
2014-04-16 15:51:48 +0100

    Added app metadata.

---
+++ src/main/java/org/eu/bitzone/application/metadata/ApplicationMetaData.java
@@ -0,0 +1,28 @@
+package org.eu.bitzone.application.metadata;
+
+import java.util.Map;
+import java.util.jar.Attributes;
+
+public class ApplicationMetaData {
+
+	private final String buildNumber;
+	private final String date;
+	private final String version;
+	private final String luceneVersionSupported;
+
+	public ApplicationMetaData(Attributes mainAttributes,
+			Map<String, Attributes> map) {
+		this.version = mainAttributes.getValue("Implementation-Version");
+		this.date = mainAttributes.getValue("Build-Time");
+		this.buildNumber = mainAttributes.getValue("Implementation-Build");
+		this.luceneVersionSupported = mainAttributes.getValue("Lucene-Version-Supported");
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Leia sister of Luke  - Lucene Index Toolbox, v [%s(%s)] (%s) Lucene version supported : %s", version,
+				buildNumber, date, luceneVersionSupported);
+	}
+
+}
